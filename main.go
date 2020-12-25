3b10c53752c1977cf2085a8ab7518e0bee0ae564
WAHWJ <duyabingyiran@163.com>
2020-12-25 10:13:34 +0800

    加入前端文件

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
 
 func main() {
 	r := gin.Default()
+	//配置模板文件的静态文件路径
+	r.Static("/static", "static")
+	//配置模板文件路径
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//待办事项
+	//添加
+	//查看
+	//修改
+	//删除
+	r.Run()
 }
